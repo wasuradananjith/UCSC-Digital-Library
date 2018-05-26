c2aeee5c0122069ec0fd3d468cef0c4743804d45
wasuradananjith <wasuradananjith@gmail.com>
2018-05-26 12:50:48 +0530

    Server created

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-
 const port = process.env.PORT || 3000; // whatever is in the environment variable PORT, or 3000
+const user = require('./routes/users');
+
+app.use(express.static(path.join(__dirname,"public"))); // front-end is in public folder
+
+app.use('/user',user);
 
 app.get("/",(req,res)=>{
     res.send("UCSC Library");
